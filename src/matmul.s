ca75f5f8b7d3caf2a91f74818d086608b0928913
RBing123 <ryan3552215@gmail.com>
2024-11-05 23:53:35 +0800

    modify matmul.s

--- src/matmul.s
+++ src/matmul.s
@@ -116,7 +116,33 @@ inner_loop_start:
     
 inner_loop_end:
     # TODO: Add your own implementation
+    mv t0, a2          # t0 = number of columns
+    li t1, 4           # size of integer
+    li t2, 0           # accumulator for offset
+offset_loop:
+    beqz t0, offset_done
+    add t2, t2, t1     # add sizeof(int) each time
+    addi t0, t0, -1
+    j offset_loop
+offset_done:
+    add s3, s3, t2     # move to next row in matrix A
+    
+    addi s0, s0, 1     # increment outer loop counter
+    j outer_loop_start
 
+outer_loop_end:
+    # Epilogue
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    
+    addi sp, sp, 28
+    
+    jr ra
 error:
     li a0, 38
     j exit
